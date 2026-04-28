4869718e1f3418585bc055cc946df6e003bc2e95
alfu-git <mdalfaz.dev@gmail.com>
2026-04-29 01:42:43 +0600

    implemented login system using google and github

--- src/components/homePage/thirdColumn/LoginSection.jsx
+++ src/components/homePage/thirdColumn/LoginSection.jsx
@@ -1,18 +1,38 @@
+"use client";
+import { authClient } from "@/lib/auth-client";
 import React from "react";
 import { FaGithub, FaGoogle } from "react-icons/fa";
 
 const LoginSection = () => {
+  const handleGoogleLogin = async () => {
+    const data = await authClient.signIn.social({
+      provider: "google",
+    });
+  };
+
+  const handleGithubLogin = async () => {
+    const data = await authClient.signIn.social({
+      provider: "github",
+    });
+  };
+
   return (
     <div className="mb-8">
       <h5 className="mb-5 text-[#403F3F] text-xl font-semibold">Login With</h5>
 
       <div className="flex flex-col gap-3">
-        <button className="btn shadow-none bg-transparent border-2 border-blue-600 text-blue-600 text-base rounded-lg">
+        <button
+          onClick={handleGoogleLogin}
+          className="btn shadow-none bg-transparent border-2 border-blue-600 text-blue-600 text-base rounded-lg"
+        >
           <FaGoogle size={18} />
           <span>Login with Google</span>
         </button>
 
-        <button className="btn shadow-none bg-transparent border-2 border-black text-base rounded-lg">
+        <button
+          onClick={handleGithubLogin}
+          className="btn shadow-none bg-transparent border-2 border-black text-base rounded-lg"
+        >
           <FaGithub size={18} />
           <span>Login with Github</span>
         </button>
